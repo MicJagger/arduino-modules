3e2736c83c5432515f5f09f5ba065ab709d8b3a7
Michael Jagiello <michaeljr623@hotmail.com>
2025-03-09 17:00:26 -0400

    added multithreading through tasking example

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 // just set the include file here to the file that you would like
 // their setup() and loop() has been renamed to Setup() and Loop()
 // that way you can quickly change this #include to the code you would like to load
-#include "HB100-doppler-radar.hpp"
+#include "multithreading-tasking.hpp"
 // also ensure you change the monitor_speed in platformio.ini if necessary
 
 // "do not touch these"
---
+++ src/multithreading-tasking.hpp
@@ -0,0 +1,56 @@
+#include <Arduino.h>
+
+TaskHandle_t task0;
+TaskHandle_t task1;
+
+void Task0(void* parameter);
+void Task1(void* parameter);
+
+
+// 
+
+void Setup() {
+  Serial.begin(115200);
+  Serial.println("Setup sequence initialized");
+
+  // creates a task that is pinned to a chosen core, and runs independently
+  xTaskCreatePinnedToCore(
+    Task0,      // function name
+    "Task0",    // name
+    10000,      // stack size
+    NULL,       // parameter(s)
+    1,          // priority
+    &task0,     // handle
+    0);         // core to pin to
+  
+  delay(100);
+
+  // technically not necessary with normal loop() code running on core 1
+  xTaskCreatePinnedToCore(Task1, "Task1", 10000, NULL, 1, &task1, 1);
+  delay(100);
+
+  Serial.println("Booted and Setup Successfully");
+}
+
+void Loop() {
+  
+}
+
+
+// 
+
+void Task0(void* parameter) {
+  while (true) {
+    Serial.print("Task0 - core ");
+    Serial.println(xPortGetCoreID());
+    delay(1000);
+  }
+}
+
+void Task1(void* parameter) {
+  while (true) {
+    Serial.print("Task1 - core ");
+    Serial.println(xPortGetCoreID());
+    delay(1050);
+  }
+}
